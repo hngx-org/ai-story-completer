0ed173d0eb8431d3c1cf12fa0fd209af2c961937
joykangangi <64706463+joykangangi@users.noreply.github.com>
2023-09-30 23:58:14 +0300

    add splash screen

--- app/src/main/java/com/newton/storycompleter/ui/theme/Type.kt
+++ app/src/main/java/com/newton/storycompleter/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.newton.storycompleter.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.newton.storycompleter.R
 
-// Set of Material typography styles to start with
+val BaskerVille = FontFamily(Font(R.font.baskervville_italic))
+val Georgian = FontFamily(Font(R.font.noto_sans_georgian))
+
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = BaskerVille,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Medium
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontFamily = Georgian,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    bodyMedium = TextStyle(
+        fontFamily = Georgian,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Georgian,
+        fontSize = 14.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.4.sp
+    ),
 )
